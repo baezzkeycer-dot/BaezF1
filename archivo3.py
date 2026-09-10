a659b46b7daafd3387e62b8ee84f864d3b3dab9d
Alex Baez <baezzkeycer@gmail.com>
2026-09-10 12:44:16 -0500

    formativa3

--- archivo3.py
+++ archivo3.py
@@ -1,4 +1,4 @@
 #GRADO Y NSA:AL3FAP 613639 
 #APELLIDOS Y NOMBRES Baez Bustamante Alex 
 #ESPELCIALIDAD Y AÑO: INFORMATICA-III AÑO
-print("Evaluacion practica ejecutada correctamente en la rama _baez3_")
+print("Evaluacion practica ejecutada correctamente en la rama baez3")
