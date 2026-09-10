1ae59cd2492920927bfc11bdcd91352a2033161f
Alex Baez <baezzkeycer@gmail.com>
2026-09-10 12:24:35 -0500

    formatica2

--- archivo2.py
+++ archivo2.py
@@ -1,4 +1,4 @@
 #GRADO Y NSA:AL3FAP 613639 
 #APELLIDOS Y NOMBRES Baez Bustamante Alex 
 #ESPELCIALIDAD Y AÑO: INFORMATICA-III AÑO
-print("Evaluacion practica ejecutada correctamente en la rama baez1.")
+print("Evaluacion practica ejecutada correctamente en la rama baez2.")
